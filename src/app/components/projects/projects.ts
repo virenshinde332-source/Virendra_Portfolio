7395bddd51606a926f9a4fa5b3ea466b00debc3a
Virendra Shinde <virenshinde332@gmail.com>
2026-06-14 12:05:15 +0530

    Project demo add

--- src/app/components/projects/projects.ts
+++ src/app/components/projects/projects.ts
@@ -8,7 +8,27 @@ import { Component } from '@angular/core';
 })
 export class Projects {
 
-  openDemo() {
+  opengit() {
+  window.open('https://github.com/virenshinde332-source/Book_Store', '_blank');
+}
+
+openDemo() {
   window.open('https://book-store-e176.onrender.com', '_blank');
+}
+
+opengit1() {
+  window.open('https://github.com/virenshinde332-source/Grocery_Store', '_blank');
+}
+
+openDemo1() {
+  window.open('https://grocery-store-blhp.onrender.com', '_blank');
+}
+
+opengit2() {
+  window.open('https://github.com/virenshinde332-source/Virendra_Portfolio', '_blank');
+}
+
+openDemo2() {
+  window.open('https://virendra-portfolio-virendra-shinde-s-projects.vercel.app/', '_blank');
 }
 }
